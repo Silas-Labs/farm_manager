5e78b57a81c04170614fa37efd08adf1bcca19e3
Silas <silaslelei@ymail.com>
2026-03-03 03:53:45 +0300

    fix: return the plant with key insensitivity in url param

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.get('/api/store', (req, res) => {
-  const filePath = path.join(__dirname, 'data', 'store.json');
-  
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) return res.status(500).json({ error: 'Could not read file' });
+app.get("/api/store/plants", (req, res) => {
+  const filePath = path.join(__dirname, "data", "store.json");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) return res.status(500).json({ error: "Could not read file" });
 
     const items = JSON.parse(data);
     const { name } = req.query;
 
     if (!name) return res.json(items);
 
-    const filtered = items.filter(item =>
-      item
+    const matchedKey = Object.keys(items.plants[0]).find(
+      (key) => key.toLowerCase() === name.toLowerCase()
     );
 
-    res.json(filtered);
+    if (!matchedKey) {
+      return res.status(404).json({ error: "Plant not found" });
+    }
+
+    return res.status(200).json(items.plants[0][matchedKey]);
   });
 });
 
