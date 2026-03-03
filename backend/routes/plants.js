43b7490ee1d09e9770260efc0ad13eca9024d7e8
Silas <silaslelei@ymail.com>
2026-03-03 04:53:44 +0300

    feat(routes): add endpoint for plant varieties

--- backend/routes/plants.js
+++ backend/routes/plants.js
@@ -7,4 +7,30 @@ router.get("/", (req, res) => {
   return res.status(200).json(store["plants"]);
 });
 
+router.get("/:plant", (req, res) => {
+  const { plant } = req.params;
+  const plants = store["plants"][0];
+    console.log("pARAM: ",plants)
+  const matchKey = Object.keys(plants).find(
+    (it) => it.toLocaleLowerCase() === plant.toLocaleLowerCase()
+  );
+
+  if (matchKey) return res.status(200).json(plants[matchKey]);
+
+  return res.status(400).json({ error: "Plant not found" });
+});
+
+router.get("/:plant/varieties", (req, res) => {
+  const { plant } = req.params;
+  const plants = store["plants"][0];
+    console.log("pARAM: ",plants)
+  const matchKey = Object.keys(plants).find(
+    (it) => it.toLocaleLowerCase() === plant.toLocaleLowerCase()
+  );
+
+  if (matchKey) return res.status(200).json(plants[matchKey]["varieties"]);
+
+  return res.status(400).json({ error: "No such plant or varieties" });
+});
+
 module.exports = router;
