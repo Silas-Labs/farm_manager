afa753893fde4b0795ff94ce1f57f554b5e68693
Silas <silaslelei@ymail.com>
2026-03-17 11:11:04 +0300

    refactor(sidebar): change menu to actual farm context

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import {
+  BicepsFlexed,
+  ChartNoAxesCombined,
+  CloudSun,
+  LayoutDashboard,
+  Sprout,
+  Tractor,
+  User2,
+  ChevronUp,
+} from "lucide-react";
 
 import {
   SidebarProvider,
@@ -15,37 +24,85 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+
 export const SideBar = () => {
   return (
     <SidebarProvider>
       <Sidebar collapsible="icon">
-        <SidebarTrigger/>
-          <SidebarHeader>My Sidebar</SidebarHeader>
-          <SidebarContent>
-            <SidebarGroup>
-              <SidebarMenu>
-                <SidebarMenuItem>
+        <SidebarTrigger />
+        <SidebarHeader />
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <LayoutDashboard />
+                  <span>
+                    <a href="#">Dashboard</a>
+                  </span>
+                </SidebarMenuButton>
+                <SidebarMenuButton>
+                  <Sprout />
+                  <span>
+                    <a href="#">Crop Management</a>
+                  </span>
+                </SidebarMenuButton>
+                <SidebarMenuButton>
+                  <CloudSun />
+                  <span>
+                    <a href="#">Weather</a>
+                  </span>
+                </SidebarMenuButton>
+                <SidebarMenuButton>
+                  <Tractor />
+                  <span>
+                    <a href="#">Equipment</a>
+                  </span>
+                </SidebarMenuButton>
+                <SidebarMenuButton>
+                  <BicepsFlexed />
+                  <span>
+                    <a href="#">Labor Management</a>
+                  </span>
+                </SidebarMenuButton>
+                <SidebarMenuButton>
+                  <ChartNoAxesCombined />
+                  <span>
+                    <a href="#">Reports and Analytics</a>
+                  </span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
                   <SidebarMenuButton>
-                    <Home/>
-                    <span><a href="#">Home</a></span>
+                    <User2 /> Username
+                    <ChevronUp className="ml-auto" />
                   </SidebarMenuButton>
-                  <SidebarMenuButton>
-                    <Inbox/>
-                    <span><a href="#">Crops</a></span>
-                  </SidebarMenuButton>
-                  <SidebarMenuButton>
-                    <Search/>
-                    <span><a href="#">Animals</a></span>
-                  </SidebarMenuButton>
-                  <SidebarMenuButton>
-                    <Settings/>
-                    <span><a href="#">Accounts</a></span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              </SidebarMenu>
-            </SidebarGroup>
-          </SidebarContent>
-          <SidebarFooter></SidebarFooter>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width]"
+                >
+                  <DropdownMenuItem>
+                    <span>Sign out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
       </Sidebar>
     </SidebarProvider>
   );
