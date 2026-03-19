748b6cef151930831b49b2c5a29969625b3929b1
Silas <writer.silas@gmail.com>
2026-03-19 21:23:56 +0300

    style(header): fix alignment to take space from sidebar to end

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="w-full border border-blue-400">Header</div>;
+  return <div className="w-screen border border-blue-400">Header</div>;
 };
 
 export default Header;
