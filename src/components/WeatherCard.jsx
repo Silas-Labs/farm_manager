eee3ff95b1202d4ad23ea6285001019a8c6622d3
Silas <silaslelei@ymail.com>
2026-03-21 02:39:47 +0300

    feat: add weather card component

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,28 +6,44 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+import { CloudSunRain } from "lucide-react";
 
 export function WeatheCard() {
+  const weather = {
+    temp: "30ºC",
+    temp_approx: "Feels like 26ºC",
+    location: "Kapsabet",
+    day: "Saturday",
+    date: "21 Mar 2026",
+    condition: "Heavy Rain",
+  };
   return (
-    <Card size="sm" className="max-w-sm">
-      <CardHeader>
-        <CardTitle>Small Card</CardTitle>
+    <Card
+      size="md"
+      className="w-[300px] max-h-[150px] flex flex-row justify-evenly "
+    >
+      <CardHeader className="flex flex-1">
         <CardDescription>
-          This card uses the small size variant.
+          <p className="text-[10px] bg-green-600 rounded-lg text-center text-white p-1">
+            {weather.location}
+          </p>
+          <CardTitle>
+            <p className="font-bold text-black">{weather.day}</p>
+            <span className="text-[11px] text-slate-400">{weather.date}</span>
+          </CardTitle>
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        <p>
-          The card component supports a size prop that can be set to
-          &quot;sm&quot; for a more compact appearance.
-        </p>
+      <div className="flex flex-1 items-center">
+        <CloudSunRain size={64} />
+      </div>
+      <CardContent className="flex  flex-col justify-between items-center">
+        <p className="text-[20px] font-bold">{weather.temp}</p>
+        <span>
+          <p className="text-[13px] font-bold">{weather.condition}</p>
+          <p className="text-[10px] text-slate-400">{weather.temp_approx}</p>
+        </span>
       </CardContent>
-      <CardFooter>
-        <Button variant="outline" size="sm" className="w-full">
-          Action
-        </Button>
-      </CardFooter>
     </Card>
-  )
+  );
 }
