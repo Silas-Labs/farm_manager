8308bc83bb69a144afe26451e8393f5958852445
Silas <silaslelei@ymail.com>
2026-03-21 08:36:33 +0300

    style: fix max-width to 1024px(5xl)

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -7,9 +7,9 @@ import {SidebarProvider} from '@/components/ui/sidebar'
 export const Layout = () => {
   return (
     <SidebarProvider>
-    <div className="flex w-screen">
-      <SideBar />
-      <div>
+    <div className="flex w-screen max-w-5xl">
+      <SideBar className="flex-1"/>
+      <div className="flex-1">
         <Header />
         <div className="border border-blue-950 w-full flex justify-items-start p-4">
           <WeatheCard />
