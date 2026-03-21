901390f00a2f1325a5c82e1a00c8e0c89ee39da3
Silas <silaslelei@ymail.com>
2026-03-21 08:38:13 +0300

    style: fix max-width to 1024px(5xl)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="w-screen border border-blue-400">Header</div>;
+  return <div className="max-w-5xl border border-blue-400">Header</div>;
 };
 
 export default Header;
