8c1b5fc10297ebcb40b021f5ff4e8a3687d9cd1a
ksilas <silaslelei@ymail.com>
2026-03-31 15:18:31 +0300

    style: move add button to end

--- frontend/src/pages/Labour.jsx
+++ frontend/src/pages/Labour.jsx
@@ -4,9 +4,9 @@ import {Button} from "@/components/ui/button"
 
 export const Labour = () => {
   return (
-    <div className='w-full flex p-1'>
-          <div className='w-full flex border border-blue-200'>
-            <Button variant='outline' className="mr-auto">Add Labor</Button>
+    <div className='w-full flex flex-col p-1'>
+          <div className='w-full flex justify-end border border-blue-200'>
+            <Button variant='outline'>Add Labor</Button>
           </div>
     
         </div>
