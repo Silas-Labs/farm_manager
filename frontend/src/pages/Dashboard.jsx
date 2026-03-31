242bb26a51fc6ebd36303be21d372c215771edac
ksilas <silaslelei@ymail.com>
2026-03-31 15:08:03 +0300

    style: fix layout and size of table nd graph

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,53 +1,84 @@
 import React from 'react'
-
-import {Chart} from "../components/Chart"
+import { Chart } from "../components/Chart"
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { TableDisplay } from '../components/Table';
 
 export const Dashboard = () => {
   return (
-    <div className='w-full flex flex-row p-4 mr-5 gap-4'>
-    <div className='flex flex-1 flex-col'>
-        {/* Farm Overview CArds */}
-    <div className='flex gap-4 w-full'>
-        <Card className='min-w-1/6 min-h-60 p-2'>
+    <div className='w-full flex gap-4 p-4'>
+      
+      {/* Left Column */}
+      <div className='flex-1 flex flex-col gap-4'>
+        
+        {/* Top KPI Cards */}
+        <div className='flex gap-4'>
+          <Card className='flex-1 p-4'>
             <CardTitle>Active Crops</CardTitle>
             <CardContent>
-                <CardTitle></CardTitle>
+              <p className='text-2xl font-bold'>12</p>
             </CardContent>
-        </Card>
-        <Card className='min-w-1/6 min-h-60 p-2'>
+          </Card>
+
+          <Card className='flex-1 p-4'>
             <CardTitle>Total Expense</CardTitle>
             <CardContent>
-                <CardTitle></CardTitle>
+              <p className='text-2xl font-bold'>$3,500</p>
             </CardContent>
-        </Card>
-        <Card className='min-w-1/6 min-h-60 p-2'>
+          </Card>
+
+          <Card className='flex-1 p-4'>
             <CardTitle>Month Revenue</CardTitle>
             <CardContent>
+              <p className='text-2xl font-bold'>$5,200</p>
             </CardContent>
+          </Card>
+        </div>
+
+        {/* Financial Chart */}
+        <Card className='p-4'>
+          <CardTitle>Expenses vs Revenue</CardTitle>
+          <CardContent>
+            <Chart />
+          </CardContent>
         </Card>
-       
-    </div>
-    {/* Financial Overview */}
-    <div className='w-1/4'>
-        <Chart/>
-    </div>
 
+        {/* Crop Status Overview */}
+        <div className='grid grid-cols-3 gap-4'>
+          <Card className='p-4'>
+            <CardTitle>Planted</CardTitle>
+            <CardContent>
+              <p className='text-xl font-semibold'>5</p>
+            </CardContent>
+          </Card>
+          <Card className='p-4'>
+            <CardTitle>Growing</CardTitle>
+            <CardContent>
+              <p className='text-xl font-semibold'>4</p>
+            </CardContent>
+          </Card>
+          <Card className='p-4'>
+            <CardTitle>Ready to Harvest</CardTitle>
+            <CardContent>
+              <p className='text-xl font-semibold'>3</p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
 
-    {/* Crops status overview */}
-    <div></div>
-    </div>
-    <div className='w-2/8'>
-    <TableDisplay/>
-    </div>
+      {/* Right Column */}
+      <div className='w-1/4'>
+        <Card className='p-4'>
+          <CardTitle>Recent Activities</CardTitle>
+          <CardContent>
+            <TableDisplay />
+          </CardContent>
+        </Card>
+      </div>
+
     </div>
   )
 }
