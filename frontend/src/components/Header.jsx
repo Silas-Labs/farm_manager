fd74054032b911dcf1dca82b0728628ce0197ed9
ksilas <silaslelei@ymail.com>
2026-04-01 12:18:08 +0300

    feat(Header): add contextual header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 
 const Header = () => {
-  return <div className="w-full min-h-12 border">Header</div>;
+  const [page, setPage] = useState("")
+  const location = useLocation()
+
+  useEffect(()=>{
+    changeHeader()
+  },[location])
+
+  const changeHeader=()=>{
+    switch (location.pathname){
+      case "/crops":
+        setPage("Crops")
+        break
+      case "/weather":
+        setPage("Weather")
+        break;
+      case "/equipment":
+        setPage("Equipment")
+        break;
+      case "/labor":
+        setPage("Labor")
+        break;
+      case "/reports":
+        setPage("Reports")
+        break
+      default:
+        setPage("Farm Manager")
+        break
+    }
+  }
+  
+  return <div className="w-full min-h-12 border flex items-center p-2 text-4xl font-medium text-green-800/90">{page}</div>;
 };
 
 export default Header;
