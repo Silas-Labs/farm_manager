af63edfe2df7d4a1f482d4c9bb4217ad88daa1f5
ksilas <silaslelei@ymail.com>
2026-04-02 10:49:41 +0300

    feat(AddCropModal): populate modal with form

--- frontend/src/components/AddCropModal.jsx
+++ frontend/src/components/AddCropModal.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from "react";
 
-import {Card,CardDescription} from "@/components/ui/card"
+import {
+  Card,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import TextField from "@mui/material/TextField";
+import dayjs from "dayjs";
 
 export const AddCropModal = ({ onClose }) => {
   return (
-    <div
-      className="fixed inset-0  flex justify-center items-center z-[99] shadow shadow-xl"
+    <form
+      className="fixed inset-0  flex justify-center items-center z-99  shadow-xl"
       onClick={onClose}
     >
-      <Card className="w-2/3 h-2/3 p-4" onClick={e => e.stopPropagation()}>
-        <CardDescription>Crop Modal</CardDescription>
-       
+      <Card className="w-1/3  p-4" onClick={(e) => e.stopPropagation()}>
+        <CardTitle>Plant Crop</CardTitle>
+        <CardDescription>
+          <div className="h-full flex flex-col gap-4">
+              <Input className="min-h-13" placeholder="Crop e.g maize" />            
+              <Input className="min-h-13" placeholder="Duration in months" />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="Select date"
+                slotProps={{
+                  textField: { fullWidth: true },
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+        </CardDescription>
+        <CardFooter className="mt-auto bg-transparent flex justify-center gap-5">
+          <Button className="min-h-12 min-w-18">Save</Button>
+          <Button className="min-h-12 min-w-18">Cancel</Button>
+        </CardFooter>
       </Card>
-    </div>
-  )
-}
+    </form>
+  );
+};
