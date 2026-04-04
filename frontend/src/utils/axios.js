38303887b6d069cf8f046719d2976aae422f5128
ksilas <silaslelei@ymail.com>
2026-04-04 17:05:17 +0300

    fix: add VITE_ to env to make them accessible

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const { url, timeout } = import.meta.env;
+const { VITE_BASE_URL, VITE_TIMEOUT, VITE_PORT } = import.meta.env;
 
-export const AXIOS = axios.create({
-  baseURL: url,
-  timeout: timeout,
+ const AXIOS = axios.create({
+  baseURL: VITE_BASE_URL + VITE_PORT,
+  timeout: VITE_TIMEOUT,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+export default AXIOS
