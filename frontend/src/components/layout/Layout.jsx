b32e4a7fad6e1c16e5c7cef96428a3077de84517
Silas Lelei <65106291+Silas-Labs@users.noreply.github.com>
2026-04-05 20:43:45 +0300

    Update Layout.jsx

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {BrowserRouter, Outlet} from 'react-router'
 import { SideBar } from "../Sidebar";
 import Header from "../Header";
-import {SidebarProvider} from '@/components/ui/sidebar'
+import {SidebarProvider, SidebarTrigger} from '@/components/ui/sidebar'
 import Weather from '../../pages/Weather';
 export const Layout = () => {
   return (
     
     <SidebarProvider>
     <div className="flex w-screen ">
+<SidebarTrigger/>
       <SideBar />
       <div className="flex-1 w-full">
         <Header />
