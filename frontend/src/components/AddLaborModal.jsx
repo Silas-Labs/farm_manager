c48011782c942496659ce99fb9bc17f41d30cffb
ksilas <silaslelei@ymail.com>
2026-04-08 11:40:35 +0300

    feat(AddLaborModal): set input fields as controlled

--- frontend/src/components/AddLaborModal.jsx
+++ frontend/src/components/AddLaborModal.jsx
@@ -14,16 +14,16 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs from "dayjs";
 
 export const AddLaborModal = ({ onClose }) => {
-    const [id,setId] = useState("")
-    const [name, setName] = useState("");
-    const [doB, setDoB] = useState("");
-    const [phone, setPhone] = useState("");
-    const [location, setLocation] = useState("");
-    const [home, setHome] = useState("");
-    const [kin, setKin] = useState("");
-    const [kinPhone, setKinPhone] = useState("");
-    const [role, setRole] = useState("");
-    const [status, setStatus] = useState("");
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [doB, setDoB] = useState(dayjs());
+  const [phone, setPhone] = useState("");
+  const [location, setLocation] = useState("");
+  const [home, setHome] = useState("");
+  const [kin, setKin] = useState("");
+  const [kinPhone, setKinPhone] = useState("");
+  const [role, setRole] = useState("");
+  const [status, setStatus] = useState("");
   return (
     <form
       className="fixed inset-0  flex justify-center items-center z-99"
@@ -43,24 +43,71 @@ export const AddLaborModal = ({ onClose }) => {
         <CardTitle>Add Labourer</CardTitle>
         <CardDescription>
           <div className="h-full flex flex-col gap-4">
-            <Input className="min-h-13" placeholder="ID Number" />
-            <Input className="min-h-13" placeholder="Name" />
+            <Input
+              className="min-h-13"
+              placeholder="ID Number"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+            />
+            <Input
+              className="min-h-13"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <DatePicker
                 label="Date of Birth"
                 slotProps={{
                   textField: { fullWidth: true },
                 }}
+                value={doB}
+                onChange={() => setDoB(() => (newDate) => newDate)}
                 maxDate={dayjs()}
               />
             </LocalizationProvider>
-            <Input className="min-h-13" placeholder="Phone Number" />
-            <Input className="min-h-13" placeholder="Location" />
-            <Input className="min-h-13" placeholder="Home" />
-            <Input className="min-h-13" placeholder="Next of Kin" />
-            <Input className="min-h-13" placeholder="Phone Number" />
-            <Input className="min-h-13" placeholder="Role" />
-            <Input className="min-h-13" placeholder="Status" />
+            <Input
+              className="min-h-13"
+              placeholder="Phone Number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <Input
+              className="min-h-13"
+              placeholder="Location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <Input
+              className="min-h-13"
+              placeholder="Home"
+              value={home}
+              onChange={(e) => setHome(e.target.value)}
+            />
+            <Input
+              className="min-h-13"
+              placeholder="Next of Kin"
+              value={kin}
+              onChange={(e) => setKin(e.target.value)}
+            />
+            <Input
+              className="min-h-13"
+              placeholder="Phone Number"
+              value={kinPhone}
+              onChange={(e) => setKinPhone(e.target.value)}
+            />
+            <Input
+              className="min-h-13"
+              placeholder="Role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            />
+            <Input
+              className="min-h-13"
+              placeholder="Status"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+            />
           </div>
         </CardDescription>
         <CardFooter className="mt-auto bg-transparent flex justify-center gap-5">
