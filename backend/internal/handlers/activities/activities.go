8c29977a06a0aa370348ac098c91d66ecd755639
ksilas <silaslelei@ymail.com>
2026-04-09 12:23:24 +0300

    refactor(activities): split route handlers to individual functions

--- backend/internal/handlers/activities/activities.go
+++ backend/internal/handlers/activities/activities.go
@@ -5,43 +5,67 @@ import (
 	"net/http"
 )
 
-type Error struct{
-	Code int
+type Error struct {
+	Code    int
 	Message string
 }
 
-func ActivitiesHandler(w http.ResponseWriter, r *http.Request) {
-	println("Chekpoint Activity")
-	if r.URL.Path == "/activities" {
-		switch(r.Method) {
-		case "GET":
-			w.WriteHeader(http.StatusOK)
-			error := Error{
-				Code: http.StatusOK,
-				Message: "OK",
-			}
-			json.NewEncoder(w).Encode(error)
+func ActivityHandler(w http.ResponseWriter, r *http.Request) {
+	println("URL working")
+	println(r.URL.Query().Get("id"))
+}
 
-		case "POST":
-			w.WriteHeader(http.StatusCreated)
-			error := Error{
-				Code: http.StatusCreated,
-				Message: "Created successfully",
-			}
-			json.NewEncoder(w).Encode(error)
-
-		default:
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			error := Error{
-				Code: http.StatusMethodNotAllowed,
-				Message: "method not allowed",
-			}
-			json.NewEncoder(w).Encode(error)
+func GetAllActivities(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
 		}
+		json.NewEncoder(w).Encode(error)
 	}
 }
 
-func ActivityHandler(w http.ResponseWriter, r *http.Request){
-	println("URL working")
-	println(r.URL.Query().Get("id"))
+func GetActivity(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+}
+
+func AddActivity(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+}
+
+func EditActivity(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "PUT" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+}
+
+func DeleteActivity(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "DELETE" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
 }
