0d27ffc37b238ad1db98b29594c64a0d71a9ae31
ksilas <silaslelei@ymail.com>
2026-04-09 11:08:37 +0300

    feat(main): add activities main handlers

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,13 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/Silas-Labs/farm_manager/backend/internal/activities"
 )
 
 func main() {
+	// Status check
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "API is running 🚀")
 	})
 
-	fmt.Println("Server running on :8080")
+	//Activities
+	http.HandleFunc("/activities", ActivitiesHandler) //POST & GET(all)
+	http.HandleFunc("/activities/:id", ActivityHandler) // PUT, DELETE & GET(single)
+
+	//Inputs
+
+	//Expenses
+
+	//Revenue
+
+	//Harvest
+
+	//Analytics
+
+	
+	fmt.Println("Server starting on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
