f3743172d1f11d50b41ace7b3ef5e7c0edaf95a8
ksilas <silaslelei@ymail.com>
2026-04-09 12:22:20 +0300

    feat(main): add chi router setup for activity routes

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,29 +5,35 @@ import (
 	"net/http"
 
 	"backend/internal/handlers/activities"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	r := chi.NewRouter()
 	// Status check
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "API is running 🚀")
 	})
 
-	//Activities
-	http.HandleFunc("/activities", activities.ActivitiesHandler) //POST & GET(all)
-	http.HandleFunc("/activities/:id", activities.ActivityHandler) // PUT, DELETE & GET(single)
+	// Activities
+	r.Route("/activities", func(r chi.Router) {
+		r.Get("/activities", activities.GetAllActivities)
+		r.Post("/activities", activities.AddActivity) 
+		r.Get("/activities/{id}", activities.GetActivity) 
+		r.Put("/activities/{id}", activities.EditActivity) 
+		r.Delete("/activities/{id}", activities.DeleteActivity) 
+	})
+	// Labor
 
-	//Inputs
+	// Expenses
 
-	//Expenses
+	// Equipment
 
-	//Revenue
+	// Crop
 
-	//Harvest
+	// Analytics
 
-	//Analytics
-
-	
 	fmt.Println("Server starting on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
