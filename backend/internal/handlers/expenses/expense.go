000924e13d9996eb7f965657c86c97c33b846b37
ksilas <silaslelei@ymail.com>
2026-04-13 13:08:49 +0300

    feat: add expense routes

---
+++ backend/internal/handlers/expenses/expense.go
@@ -0,0 +1,63 @@
+package expenses
+
+import (
+	"backend/internal/models"
+	"encoding/json"
+	"net/http"
+)
+
+
+func GetAllExpenses(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+}
+
+func GetExpense(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+}
+
+func AddExpense(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+}
+
+func EditExpense(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "PUT" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+}
+
+func DeleteExpense(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "DELETE" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+}
