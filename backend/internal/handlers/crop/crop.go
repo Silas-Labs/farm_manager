cd90f087dea26bdeaa6fca8abde32d918f80b7af
Silas <silaslelei@ymail.com>
2026-04-15 11:12:16 +0300

    feat: add crop handler

--- backend/internal/handlers/crop/crop.go
+++ backend/internal/handlers/crop/crop.go
@@ -1,1 +1,73 @@
 package crop
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"backend/internal/models"
+)
+
+func ActivityHandler(w http.ResponseWriter, r *http.Request) {
+	println("URL working")
+	println(r.URL.Query().Get("id"))
+}
+
+func GetAllCrops(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	json.NewEncoder(w).Encode({"code":http.StatusOK,"message":"Success"})
+}
+
+func GetCrop(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	json.NewEncoder(w).Encode({"code":http.StatusOK,"message":"Success"})
+}
+
+func AddCrop(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	json.NewEncoder(w).Encode({"code":http.StatusOK,"message":"Success"})
+}
+
+func EditCrop(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "PUT" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	json.NewEncoder(w).Encode({"code":http.StatusOK,"message":"Success"})
+}
+
+func DeleteCrop(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "DELETE" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	json.NewEncoder(w).Encode({"code":http.StatusOK,"message":"Success"})
+}
