08f56c25fdda1a43c415e809f6f3d75c565e00e7
ksilas <silaslelei@ymail.com>
2026-04-15 16:22:36 +0300

    feat: add labor handlers

--- backend/internal/handlers/labor/labor.go
+++ backend/internal/handlers/labor/labor.go
@@ -1,1 +1,88 @@
 package labor
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"backend/internal/models"
+)
+
+func GetAllLabor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	response := models.Response{
+		Code:    http.StatusOK,
+		Message: "Success",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func GetLabor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	response := models.Response{
+		Code:    http.StatusOK,
+		Message: "Success",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func AddLabor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	response := models.Response{
+		Code:    http.StatusOK,
+		Message: "Success",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func EditLabor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "PUT" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	response := models.Response{
+		Code:    http.StatusOK,
+		Message: "Success",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func DeleteLabor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "DELETE" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		error := models.Error{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "method not allowed",
+		}
+		json.NewEncoder(w).Encode(error)
+	}
+	response := models.Response{
+		Code:    http.StatusOK,
+		Message: "Success",
+	}
+	json.NewEncoder(w).Encode(response)
+}
