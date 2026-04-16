d2366c358603874972d865c24a50fb9b4bf9812e
ksilas <silaslelei@ymail.com>
2026-04-16 09:33:50 +0300

    feat(models): add labor model

---
+++ backend/internal/models/labor.go
@@ -0,0 +1,14 @@
+package models
+
+type Labor struct {
+	ID_Number string
+	Name      string
+	DoB       string
+	Phone     string
+	Location  string
+	Home      string
+	Kin       string
+	KinPhone  string
+	Role      string
+	Status    string
+}
