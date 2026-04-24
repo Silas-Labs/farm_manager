c7f8818f790621da18449088c77b9f62fe0b602c
Silas Lelei <65106291+Silas-Labs@users.noreply.github.com>
2026-04-24 23:24:38 +0300

    Add agricultural color palette for the Farm Manager application

---
+++ frontend/src/theme/colors.js
@@ -0,0 +1,29 @@
+const colors = {
+  crops: {
+    lightGreen: '#A8D5BA',
+    mediumGreen: '#648C5B',
+    darkGreen: '#4B8F8C'
+  },
+  soil: {
+    lightBrown: '#C2B280',
+    mediumBrown: '#B68B5E',
+    darkBrown: '#7B5A3A'
+  },
+  sky: {
+    lightBlue: '#AEEEEE',
+    mediumBlue: '#66B2D6',
+    darkBlue: '#005A8E'
+  },
+  harvest: {
+    lightGold: '#FCEBAE',
+    mediumGold: '#F2C94C',
+    darkGold: '#B57D23'
+  },
+  accents: {
+    orange: '#FF8C00',
+    red: '#C0392B',
+    yellow: '#F4D03F'
+  }
+};
+
+export default colors;
