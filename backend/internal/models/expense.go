d2c9f80b9a526bcf3f61f5567dce2f3bf778d4a4
Silas <writer.silas@gmail.com>
2026-05-10 17:33:43 +0300

    models: update Expense model

--- backend/internal/models/expense.go
+++ backend/internal/models/expense.go
@@ -1,13 +1,15 @@
 package models
 
+import "time"
+
 type Expense struct {
-	Title     string
-	Amount    string
-	Date string
-	Category string
-	Type     string
-	Brand string
-	Quantity int
-	Unit_Price float64
-	Notes string
+	ID          int       `json:"id"`
+	UserID      int       `json:"user_id"`
+	Title       string    `json:"title"`
+	Amount      float64   `json:"amount"`
+	Category    string    `json:"category"`
+	ExpenseType string    `json:"expense_type"`
+	Date        time.Time `json:"date"`
+	Notes       string    `json:"notes"`
+	CreatedAt   time.Time `json:"created_at"`
 }
