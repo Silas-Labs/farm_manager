bde008d9efdf0605a32697602420197a6a68f51d
Silas <writer.silas@gmail.com>
2026-05-10 17:34:29 +0300

    feat: frontend: add/update frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,7 @@ import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
       <App />
       <Toaster position="top-center" />
-    </BrowserRouter>
   </StrictMode>,
 );
