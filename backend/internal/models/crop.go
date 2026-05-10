840b8ca8b0a8a23c6b3e5f55ba96c30702e553c9
Silas <writer.silas@gmail.com>
2026-05-10 17:33:43 +0300

    models: update Crop model

--- backend/internal/models/crop.go
+++ backend/internal/models/crop.go
@@ -1,9 +1,17 @@
 package models
 
+import "time"
+
 type Crop struct {
-	Name     string
-	Brand    string
-	Varitery string
-	Duration int
-	Date     string
+	ID                  int       `json:"id"`
+	UserID              int       `json:"user_id"`
+	Name                string    `json:"name"`
+	Brand               string    `json:"brand"`
+	Variety             string    `json:"variety"`
+	Duration            int       `json:"duration"`
+	Stage               string    `json:"stage"`
+	PlantedDate         time.Time `json:"planted_date"`
+	ExpectedHarvestDate time.Time `json:"expected_harvest_date"`
+	CreatedAt           time.Time `json:"created_at"`
+	UpdatedAt           time.Time `json:"updated_at"`
 }
