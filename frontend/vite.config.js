8fa915d31a90dac8fc45ac030bf1e69013cad08d
ksilas <silaslelei@ymail.com>
2026-05-12 09:16:11 +0300

    fix: add proxy for CORS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      "/": {
+        target: "https://agropulse-bakcend.onrender.com/",
+        changeOrigin: true,
+      },
+    },
+  },
+});
 // Project: Farm Manager
